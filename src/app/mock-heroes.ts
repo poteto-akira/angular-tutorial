89688077d0f22f38e8f26b533de9c9712ab2fe93
poteto-akira <poteto.akira@gmail.com>
2019-01-05 02:33:33 +0900

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr.Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'hoge' },
+  { id: 14, name: 'akire' },
+  { id: 15, name: 'horio' },
+  { id: 16, name: 'hogehoge' },
+  { id: 17, name: 'riki' },
+  { id: 18, name: 'Mr.Nice' },
+  { id: 19, name: 'Mr.Nice' },
+  { id: 20, name: 'Mr.Nice' }
+]
